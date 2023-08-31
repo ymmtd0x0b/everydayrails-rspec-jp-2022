e711b92cba03c4798cfed28859297cff6c0174a7
ymmtd0x0b <ymmtd0x0b@gmail.com>
2023-08-31 12:27:11 +0900

    プロジェクト編集のスペックを作成＆処理の一部を DRY にした

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -1,26 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe "Projects", type: :system do
-  scenario "user creates a new project" do
-    user = FactoryBot.create(:user)
-    # using our custom login helper:
-    # sign_in_as user
-    # or the one provided by Devise:
-    sign_in user
+  let(:user) { FactoryBot.create(:user) }
 
+  scenario "user creates a new project" do
+    sign_in user
     visit root_path
 
     expect {
-      click_link "New Project"
-      fill_in "Name", with: "Test Project"
-      fill_in "Description", with: "Trying out Capybara"
-      click_button "Create Project"
+      create_new_project(
+        name: 'Test Project',
+        description: 'Trying out Capybara'
+      )
 
-      aggregate_failures do
-        expect(page).to have_content "Project was successfully created"
-        expect(page).to have_content "Test Project"
-        expect(page).to have_content "Owner: #{user.name}"
-      end
+      expect_successfully_saved_project(
+        flash_message: 'Project was successfully created',
+        name: 'Test Project',
+        description: 'Trying out Capybara',
+        owner: user
+      )
     }.to change(user.projects, :count).by(1)
+  end
+
+  scenario "user edits the project" do
+    project = FactoryBot.create(:project, owner: user)
+
+    sign_in user
+    visit root_path
+
+    click_link project.name
+
+    edit_project(
+      name: 'Edit Project',
+      description: 'edit description'
+    )
+
+    expect_successfully_saved_project(
+      flash_message: 'Project was successfully updated',
+      name:          'Edit Project',
+      description:   'edit description',
+      owner:          user
+    )
+  end
+end
+
+def create_new_project(name:, description:)
+  click_link "New Project"
+  fill_in "Name", with: name
+  fill_in "Description", with: description
+  click_button "Create Project"
+end
+
+def edit_project(name:, description:)
+  click_link "Edit"
+  fill_in "Name", with: name
+  fill_in "Description", with: description
+  click_button "Update Project"
+end
+
+def expect_successfully_saved_project(flash_message:, name:, description:, owner:)
+  aggregate_failures do
+    expect(page).to have_content flash_message
+    expect(page).to have_content name
+    expect(page).to have_content "Owner: #{owner.name}"
   end
 end
